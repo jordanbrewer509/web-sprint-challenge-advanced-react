5f5d1215535aba18ee32a902f12b91e1c4082a1b
Jordan~ <jordan.brewer509@gmail.com>
2023-02-13 15:08:04 -0800

    grid is fully functional

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -7,12 +7,15 @@ const initialEmail = ''
 const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 const gridArr = [[0, 1, 2],[3, 4, 5],[6, 7, 8]];
-let coordinate = {x: 2, y: 2};
+const initialCoordinate = {x: 2, y: 2};
+let steps = 0;
 
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
   const [index, setIndex] = useState(initialIndex);
+  const [coordinate, setCoordinate] = useState(initialCoordinate)
+  const [message, setMessage] = useState(initialMessage)
 
   function getXY(index) {
     // It it not necessary to have a state to track the coordinates.
@@ -21,30 +24,15 @@ export default function AppFunctional(props) {
       for(let j = 0; j < gridArr.length; j++) {
         if(gridArr[i][j] === index) {
           coordinate.x = parseInt(coordinate.x + [i + 1]);
-          coordinate.y = parseInt(coordinate.y + [i + 1]);
+          coordinate.y = parseInt(coordinate.y + [j + 1]);
           return coordinate;
         }
         }
       }
     }
 
-  function getXYMessage() {
-    // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
-    // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
-    // returns the fully constructed string.
-    if((coordinate.x + 1) > 3) {
-      return "You can't go farther right";
-    } else if((coordinate.x - 1) < 0) {
-      return "You can't go farther left";
-    } else if((coordinate.y + 1) > 3) {
-      return "You can't go any further down";
-    } else if((coordinate.y - 1) < 0) {
-      return "You can't go any higher up";
-    } 
-  }
-
-  function getIndex() {
-    // I will set setIndex at the end of this function
+  function getIndex(coordinate) {
+    // I will use setIndex at the end of this function
     // To allow me to swap the B around
     if(coordinate.x === 1 && coordinate.y === 1) {
       return setIndex(0)
@@ -69,39 +57,50 @@ export default function AppFunctional(props) {
 
   function reset() {
     // Use this helper to reset all states to their initial values.
-    coordinate.x = 2;
-    coordinate.y = 2;
-    console.log(coordinate)
+    setCoordinate({x: 2, y: 2});
+    setIndex(4);
+    steps = 0;
+    setMessage(initialMessage);
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
-    if(direction === "up" && coordinate.y > 1) {
-      coordinate.y = coordinate.y - 1; 
-      console.log(coordinate)
+    if(direction === "up" && coordinate.y > 0) {
+      if(coordinate.y <= 1) {
+        return setMessage("You can't go any higher up");
+      }
+      steps++;
+      coordinate.y = coordinate.y - 1;
+      getIndex(coordinate)
       return coordinate;
-    } else if(direction === "down" && coordinate.y < 3) {
+    } else if(direction === "down") {
+      if(coordinate.y >= 3) {
+        return setMessage("You can't go any further down");
+      }
+      steps++;
       coordinate.y = coordinate.y + 1;
-      console.log(coordinate)
+      getIndex(coordinate)
       return coordinate;
-    } else if(direction === "left" && coordinate.x > 1) {
+    } else if(direction === "left") {
+        if(coordinate.x <= 1) {
+          return setMessage("You can't go farther left");
+        }
+      steps++;
       coordinate.x = coordinate.x - 1;
-      console.log(coordinate)
+      getIndex(coordinate)
       return coordinate;
-    } else if(direction === "right" && coordinate.x < 3) {
+    } else if(direction === "right") {
+        if(coordinate.x >= 3) {
+          return setMessage("You can't go farther right");
+        }
+      steps++;
       coordinate.x = coordinate.x + 1;
-      console.log(coordinate)
+      getIndex(coordinate)
       return coordinate;
     }
   }
-
-  function move(evt) {
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
-    
-}
 
   function onChange(evt) {
     // You will need this to update the value of the input.
@@ -116,7 +115,7 @@ export default function AppFunctional(props) {
     <div id="wrapper" className={props.className}>
       <div className="info">
         <h3 id="coordinates">{`Coordinate ${coordinate.x}, ${coordinate.y}`}</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="steps">{`You moved ${steps} times`}</h3>
       </div>
       <div id="grid">
         {
@@ -128,7 +127,7 @@ export default function AppFunctional(props) {
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
         <button id="left" onClick={onChange}>LEFT</button>
